18d5ef282d72ee0ca36b258d12b3a8379161b0a3
Mitchell Lutzke <mitchell649@hotmail.com>
2014-01-06 13:42:31 -0600

    YOU GON' NEED NODE

---
+++ node/test.js
@@ -0,0 +1,28 @@
+
+var express = require('express');
+var app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+// routing
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+// usernames which are currently connected to the chat
+var pos = {};
+
+io.sockets.on('connection', function (socket) {
+
+  socket.on('mousepos', function(mouse_coords){
+    pos = mouse_coords;
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function(){
+   pos = {x: 0, y: 0};
+  });
+});
